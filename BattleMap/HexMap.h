1adfda19c03fdb05f1849081bb832c6467e5749d
Vincent Nguyen <atermagus312@gmail.com>
2017-04-02 19:55:06 -0700

    init

---
+++ BattleMap/HexMap.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <cmath>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+using std::abs;
+using std::max;
+using std::vector;
+
+const double PI = 3.14;
+class HexMap
+{
+public:
+	HexMap();
+	~HexMap();
+};
+
---
+++ BattleMap/main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+#include "HexMap.cpp"
+
+static const int MAP_SIZE = 10; 
+static const Point ORIGIN = Point(0, 0);
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+
+	Layout pointy = Layout(layout_pointy, Point(MAP_SIZE, MAP_SIZE), ORIGIN);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}	
+	return 0;
+}
